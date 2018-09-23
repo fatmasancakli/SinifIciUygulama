eff8932300469c8c6cfafa6906d8b503b280b710
vektorel <vektorel@lab2-3>
2018-09-23 21:04:55 +0300

    Admin Panel eklendi. AnaSayfaya tema olarak okullar eklendi.

--- src/com/vektorel/Beans/indexBean.java
+++ src/com/vektorel/Beans/indexBean.java
@@ -1,5 +1,79 @@
 package com.vektorel.Beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.vektorel.Models.tbldefault;
+import com.vektorel.Models.tblokullar;
+
+@SuppressWarnings("deprecation")
+@ManagedBean(name="indexBean")
+@SessionScoped
 public class indexBean {
+
+	private String ekranyazisi = "ilk JSF";
+
+	private List<tbldefault> liste = new ArrayList<>();
+	
+	private List<tblokullar> okullistesi = new ArrayList<>();
+	
+	private tblokullar okulgiris = new tblokullar();
+	
+	public void okulekle() {
+		
+		okullistesi.add(okulgiris);
+	
+		
+	}
+	
+	
+	public tblokullar getOkulgiris() {
+		return okulgiris;
+	}
+
+	public void setOkulgiris(tblokullar okulgiris) {
+		this.okulgiris = okulgiris;
+	}
+
+	public List<tblokullar> getOkullistesi() {
+		return okullistesi;
+	}
+
+	public void setOkullistesi(List<tblokullar> okullistesi) {
+		this.okullistesi = okullistesi;
+	}
+
+	public indexBean() {
+		
+		tblokullar okul;
+		okul = new tblokullar();
+		okul.setId(1L);
+		okul.setOkuladi("A.Kalpler Anaokulu");
+		okul.setOkuladresi("Ankara/Çankaya");
+		okul.setOkulwebsiteadresi("https://okul.com.tr/anaokulu/ankara/ozel-asil-kalpler-anaokulu-82");
+		okul.setResimurl("https://cdn.okul.com.tr/schools/82/1080x0/ozel-asil-kalpler-anaokulu-1.jpg");
+		okul.setOkultelefonnumarasi("03124765606");
+		okullistesi.add(okul);
+		
+	}
+	
+	public List<tbldefault> getListe() {
+		return liste;
+	}
+
+	public void setListe(List<tbldefault> liste) {
+		this.liste = liste;
+	}
+
+	public String getEkranyazisi() {
+		return ekranyazisi;
+	}
+
+	public void setEkranyazisi(String ekranyazisi) {
+		this.ekranyazisi = ekranyazisi;
+	}
 
 }
---
+++ src/com/vektorel/Models/tblokullar.java
@@ -0,0 +1,49 @@
+package com.vektorel.Models;
+
+public class tblokullar {
+
+	private long id;
+	private String okuladi;
+	private String resimurl;
+	private String okuladresi;
+	private String okulwebsiteadresi;
+	private String okultelefonnumarasi;
+	
+	public String getOkultelefonnumarasi() {
+		return okultelefonnumarasi;
+	}
+	public void setOkultelefonnumarasi(String okultelefonnumarasi) {
+		this.okultelefonnumarasi = okultelefonnumarasi;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getOkuladi() {
+		return okuladi;
+	}
+	public void setOkuladi(String okuladi) {
+		this.okuladi = okuladi;
+	}
+	public String getResimurl() {
+		return resimurl;
+	}
+	public void setResimurl(String resimurl) {
+		this.resimurl = resimurl;
+	}
+	public String getOkuladresi() {
+		return okuladresi;
+	}
+	public void setOkuladresi(String okuladresi) {
+		this.okuladresi = okuladresi;
+	}
+	public String getOkulwebsiteadresi() {
+		return okulwebsiteadresi;
+	}
+	public void setOkulwebsiteadresi(String okulwebsiteadresi) {
+		this.okulwebsiteadresi = okulwebsiteadresi;
+	}
+	
+}
